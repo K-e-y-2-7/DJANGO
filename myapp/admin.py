e6f04f38dc88a6d5cb0eac1c0a4ed3ced4673278
Key27 <Desertod2014@mail.ua>
2022-02-05 16:11:43 +0200

    Сделал 1 и 2 задание из домашнего задания №30

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,24 @@
+from logging import Filter
 from django.contrib import admin
+from .models import Books, Author, BookReaders
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
+    list_filter = ['name', 'surname']
+
+
+@admin.register(BookReaders)
+class BookReadersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
+    list_filter = ['name', 'surname']
+
+
+@admin.register(Books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year_of_published', 'are_available']
+    list_filter = ['name', 'year_of_published']
+
+
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,35 @@
+from os import name
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Author (models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name', 'surname']
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.surname}'
+
+class BookReaders (models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+
+    class Meta:
+            ordering = ['name', 'surname']
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.surname}'
+
+class Books (models.Model):
+    name = models.CharField(max_length=100)
+    year_of_published = models.DateField()
+    author = models.ManyToManyField(Author)
+    readers = models.ManyToManyField(BookReaders, null=True, blank=True)
+    are_available = models.BooleanField(default=True)
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return f'Name of the book: {self.name}; Date of published: {self.year_of_published}; Author of the book: {self.author}'
