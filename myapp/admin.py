508b0beadb1e37ed9ade9352a3e27e4c06947ead
Key27 <Desertod2014@mail.ua>
2022-02-05 17:26:42 +0200

    Completed hw2!

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,24 +1,51 @@
 from logging import Filter
 from django.contrib import admin
-from .models import Books, Author, BookReaders
+import myapp.models
 
+#==============================================================================
+#                                   LIBRARY
+#==============================================================================
 
-@admin.register(Author)
+@admin.register(myapp.models.BookAuthor)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name', 'surname']
     list_filter = ['name', 'surname']
 
 
-@admin.register(BookReaders)
+@admin.register(myapp.models.BookReaders)
 class BookReadersAdmin(admin.ModelAdmin):
     list_display = ['name', 'surname']
     list_filter = ['name', 'surname']
 
 
-@admin.register(Books)
+@admin.register(myapp.models.Books)
 class BooksAdmin(admin.ModelAdmin):
     list_display = ['name', 'year_of_published', 'are_available']
     list_filter = ['name', 'year_of_published']
 
 
+#==============================================================================
+#                                   BLOG-SITE
+#==============================================================================
 
+@admin.register(myapp.models.ArticleAuthor)
+class ArticleAuthorAdmin(admin.ModelAdmin):
+    list_display = ['pseudonym']
+    list_filter = ['name', 'pseudonym']
+
+@admin.register(myapp.models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['author', 'headline', 'text', 'genre', 'created_at', 'updated_at']
+    list_filter = ['author', 'headline', 'genre', 'created_at',]
+@admin.register(myapp.models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'text', 'comment', 'article', 'created_at', 'updated_at']
+    list_filter = ['user', 'comment', 'article', 'created_at',]
+@admin.register(myapp.models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['article', 'comment', 'user']
+    list_filter = ['user', 'comment', 'article',]
+@admin.register(myapp.models.Dislike)
+class DislikeAdmin(admin.ModelAdmin):
+    list_display = ['article', 'comment', 'user']
+    list_filter = ['user', 'comment', 'article',]
