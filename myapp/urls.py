d5fe2f0a8966dd5d22fae5710397cde34adfc622
Key27 <Desertod2014@mail.ua>
2022-02-15 07:23:03 +0200

    create new migrations and new update models. Create view logic with templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.articles, name='articles'),
     path('archive',views.archive_articles, name='archive'),
     path('<int:article_number>/', views.article, name='article'),
+    path('<int:article_number>/comments', views.article_with_comments, name='article-with-comments'),
     path('<int:article_number>/archive', views.archive_article, name='article-in-archive'),
     path('<int:article_number>/<slug:name>', views.article, name='article-name'),
 ]
